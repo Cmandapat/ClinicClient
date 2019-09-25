030f1b7c3d33264b54694beecd34242bb1310039
Christian Mandapat <cmandapat1215@gmail.com>
2019-09-25 10:29:31 -0400

    updating doctor display in patient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,78 +1,101 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Location } from '@angular/common';
-import { UserLoginService } from './userlogin.service';
-import { UserCred } from './model/usercred';
+import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Location } from "@angular/common";
+import { UserLoginService } from "./userlogin.service";
+import { UserCred } from "./model/usercred";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ClinicClient';
-  user:UserCred;
-  constructor(private router: Router, private activeRouter: ActivatedRoute, private location:Location, private userLoginService:UserLoginService){}
+  title = "ClinicClient";
+  user: UserCred;
+  constructor(
+    private router: Router,
+    private activeRouter: ActivatedRoute,
+    private location: Location,
+    private userLoginService: UserLoginService
+  ) {}
 
-  isDoctorsRouteActivated(): boolean{
-    return this.location.path().indexOf('/doctors') > -1;
-  }
-  
-  isUserRouteActivated(): boolean{
-    return (this.location.path().indexOf('/user') > -1);
-  }
-
-  isUsersRouteActivated(): boolean{
-    return (this.location.path().indexOf('/users') > -1);
+  isDoctorsRouteActivated(): boolean {
+    return this.location.path().indexOf("/doctors") > -1;
   }
 
-  isAdminRouteActivated(): boolean{
-    return ((this.location.path().indexOf('/admin') > -1) || (this.location.path().indexOf('/doctors') > -1));
+  isUserRouteActivated(): boolean {
+    return this.location.path().indexOf("/user") > -1;
   }
-  
- 
-  isAdminNotHome(): boolean{
-    if (((this.location.path().indexOf('/users') > -1) || (this.location.path().indexOf('/doctoradd') > -1) 
-    || (this.location.path().indexOf('/doctoredit') > -1) || (this.location.path().indexOf('/appointment1') > -1)
-    || (this.location.path().indexOf('/appointment') > -1) || (this.location.path().indexOf('/appointment2') > -1)
-    ) && (!(this.location.path().indexOf('/admin') > -1)) && (this.userLoginService.adminLoggedIn)){
+
+  isUsersRouteActivated(): boolean {
+    return this.location.path().indexOf("/users") > -1;
+  }
+
+  isAdminRouteActivated(): boolean {
+    return (
+      this.location.path().indexOf("/admin") > -1 ||
+      this.location.path().indexOf("/doctors") > -1
+    );
+  }
+
+  isAdminNotHome(): boolean {
+    if (
+      (this.location.path().indexOf("/users") > -1 ||
+        this.location.path().indexOf("/doctoradd") > -1 ||
+        this.location.path().indexOf("/doctoredit") > -1 ||
+        this.location.path().indexOf("/appointment1") > -1 ||
+        this.location.path().indexOf("/appointment") > -1 ||
+        this.location.path().indexOf("/appointment2") > -1) &&
+      !(this.location.path().indexOf("/admin") > -1) &&
+      this.userLoginService.adminLoggedIn
+    ) {
       return true;
-    } else 
-      return false;
+    } else return false;
   }
 
-  isAnyUserRouteActivatedAfterLogin(): boolean{
-    if(this.areLoginOrCreateActive() || this.isNoRouteActivated()){
+  isAnyUserRouteActivatedAfterLogin(): boolean {
+    if (this.areLoginOrCreateActive() || this.isNoRouteActivated()) {
       return false;
     } else {
       return true;
     }
   }
 
-  doShowAppointment(): boolean{
-    if(this.areLoginOrCreateActive() || this.isNoRouteActivated()){
+  doShowAppointment(): boolean {
+    if (this.areLoginOrCreateActive() || this.isNoRouteActivated()) {
       return false;
-    } else if ((this.location.path().indexOf('/appointment1') > -1) || (this.location.path().indexOf('/reporter') > -1)){
+    } else if (
+      this.location.path().indexOf("/appointment1") > -1 ||
+      this.location.path().indexOf("/reporter") > -1
+    ) {
       return false;
     } else {
       return true;
     }
   }
 
-  areLoginOrCreateActive(): boolean{
-    return ((this.location.path().indexOf('/login') > -1) || (this.location.path().indexOf('/new') > -1));
+  areLoginOrCreateActive(): boolean {
+    return (
+      this.location.path().indexOf("/login") > -1 ||
+      this.location.path().indexOf("/new") > -1
+    );
   }
 
-  isNoRouteActivated(): boolean{
-    if((this.location.path().indexOf('/doctors') > -1) || (this.location.path().indexOf('/users') > -1) 
-    || (this.location.path().indexOf('/user') > -1) || (this.location.path().indexOf('/new') > -1) 
-    || (this.location.path().indexOf('/create') > -1) || (this.location.path().indexOf('/login') > -1) 
-    || (this.location.path().indexOf('/appointment') > -1) || (this.location.path().indexOf('/doctoredit') > -1) 
-    || (this.location.path().indexOf('/doctoradd') > -1) || (this.location.path().indexOf('/reporter') > -1)
-    || (this.location.path().indexOf('/admin') > -1)){
-        return false;
-    }
-      else 
-        return true;
+  isNoRouteActivated(): boolean {
+    if (
+      this.location.path().indexOf("/doctors") > -1 ||
+      this.location.path().indexOf("/users") > -1 ||
+      this.location.path().indexOf("/user") > -1 ||
+      this.location.path().indexOf("/new") > -1 ||
+      this.location.path().indexOf("/create") > -1 ||
+      this.location.path().indexOf("/login") > -1 ||
+      this.location.path().indexOf("/appointment") > -1 ||
+      this.location.path().indexOf("/doctoredit") > -1 ||
+      this.location.path().indexOf("/doctoradd") > -1 ||
+      this.location.path().indexOf("/reporter") > -1 ||
+      this.location.path().indexOf("/admin") > -1
+    ) {
+      return false;
+    } else return true;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,52 +9,48 @@ import { DoctorListComponent } from "./doctor-list/doctor-list.component";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 import { TooltipModule } from "ngx-bootstrap/tooltip";
 import { ModalModule } from "ngx-bootstrap/modal";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { DatePipe } from '@angular/common';
+import { DatePipe } from "@angular/common";
 
+import { AdminDeleteDoctorComponent } from "./admin-delete-doctor/admin-delete-doctor.component";
 
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { UserProfileListComponent } from "./user-profile-list/user-profile-list.component";
+import { UserCreateComponent } from "./user-create/user-create.component";
+import { UserLoginComponent } from "./user-login/user-login.component";
+import { AdminComponent } from "./admin/admin.component";
+import { AdminUpdateDoctorComponent } from "./adminUpdateDoctor/adminUpdateDoctor.component";
+import { ApptDetailsComponent } from "./appt-details/appt-details.component";
+import { ApptListComponent } from "./appt-list/appt-list.component";
+import { CreateApptComponent } from "./create-appt/create-appt.component";
 
+import { ApptUpdateComponent } from "./appt-update/appt-update.component";
 
-
-import { AdminDeleteDoctorComponent } from './admin-delete-doctor/admin-delete-doctor.component';
-
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { UserProfileListComponent } from './user-profile-list/user-profile-list.component';
-import { UserCreateComponent } from './user-create/user-create.component';
-import { UserLoginComponent } from './user-login/user-login.component';
-import { AdminComponent } from './admin/admin.component';
-import { AdminUpdateDoctorComponent } from './adminUpdateDoctor/adminUpdateDoctor.component';
-import { ApptDetailsComponent } from './appt-details/appt-details.component';
-import { ApptListComponent } from './appt-list/appt-list.component';
-import { CreateApptComponent } from './create-appt/create-appt.component';
-
-import { ApptUpdateComponent } from './appt-update/appt-update.component';
-
-
-
-import { UserLogoutComponent } from './user-logout/user-logout.component';
-import { ReporterComponent } from './reporter/reporter.component';
-import { AdminGetDoctorSpecComponent } from './admin-get-doctor-spec/admin-get-doctor-spec.component';
+import { UserLogoutComponent } from "./user-logout/user-logout.component";
+import { ReporterComponent } from "./reporter/reporter.component";
+import { AdminGetDoctorSpecComponent } from "./admin-get-doctor-spec/admin-get-doctor-spec.component";
 
 @NgModule({
-  declarations: [AppComponent, 
-    DoctorDetailsComponent, 
-    DoctorListComponent, 
-    UserProfileComponent, 
-    UserProfileListComponent, 
-    UserCreateComponent, 
-    UserLoginComponent, 
+  declarations: [
+    AppComponent,
+    DoctorDetailsComponent,
+    DoctorListComponent,
+    UserProfileComponent,
+    UserProfileListComponent,
+    UserCreateComponent,
+    UserLoginComponent,
     AdminComponent,
-    AdminUpdateDoctorComponent, 
-    ApptDetailsComponent, 
-    ApptListComponent, 
-    CreateApptComponent, 
-    UserLogoutComponent, 
+    AdminUpdateDoctorComponent,
+    ApptDetailsComponent,
+    ApptListComponent,
+    CreateApptComponent,
+    UserLogoutComponent,
     ReporterComponent,
-    AdminDeleteDoctorComponent, 
+    AdminDeleteDoctorComponent,
     AdminGetDoctorSpecComponent,
-          ApptUpdateComponent],
-
+    ApptUpdateComponent
+  ],
 
   imports: [
     BrowserModule,
@@ -64,6 +60,7 @@ import { AdminGetDoctorSpecComponent } from './admin-get-doctor-spec/admin-get-d
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
+    MatToolbarModule
     //DatePipe
   ],
   providers: [DatePipe],
