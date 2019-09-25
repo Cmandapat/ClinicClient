e44a36f9c0b60487943eeb1e466343859a92e4c3
Zechariah Roberson <zechariah.roberson@DRMAIN.local>
2019-09-25 10:02:07 -0400

    Users can no longer see back to admin link, reporter can no longer see make appointment link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { UserLoginService } from './userlogin.service';
+import { UserCred } from './model/usercred';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,8 @@ import { Location } from '@angular/common';
 })
 export class AppComponent {
   title = 'ClinicClient';
-  constructor(private router: Router, private activeRouter: ActivatedRoute, private location:Location){}
+  user:UserCred;
+  constructor(private router: Router, private activeRouter: ActivatedRoute, private location:Location, private userLoginService:UserLoginService){}
 
   isDoctorsRouteActivated(): boolean{
     return this.location.path().indexOf('/doctors') > -1;
@@ -30,16 +33,28 @@ export class AppComponent {
  
   isAdminNotHome(): boolean{
     if (((this.location.path().indexOf('/users') > -1) || (this.location.path().indexOf('/doctoradd') > -1) 
-    || (this.location.path().indexOf('/doctoredit') > -1) || (this.location.path().indexOf('/appointment') > -1)) 
-    && (!(this.location.path().indexOf('/admin') > -1))){
+    || (this.location.path().indexOf('/doctoredit') > -1) || (this.location.path().indexOf('/appointment1') > -1)
+    || (this.location.path().indexOf('/appointment') > -1) || (this.location.path().indexOf('/appointment2') > -1)
+    ) && (!(this.location.path().indexOf('/admin') > -1)) && (this.userLoginService.adminLoggedIn)){
       return true;
-    }
+    } else 
+      return false;
   }
 
   isAnyUserRouteActivatedAfterLogin(): boolean{
     if(this.areLoginOrCreateActive() || this.isNoRouteActivated()){
       return false;
-    }else {
+    } else {
+      return true;
+    }
+  }
+
+  doShowAppointment(): boolean{
+    if(this.areLoginOrCreateActive() || this.isNoRouteActivated()){
+      return false;
+    } else if ((this.location.path().indexOf('/appointment1') > -1) || (this.location.path().indexOf('/reporter') > -1)){
+      return false;
+    } else {
       return true;
     }
   }
